9935f926471801e4803540a022b9f2205f278f7c
VISHNUK006 <22je1083@iitism.ac.in>
2025-06-23 22:31:50 +0530

    Update database config for render deployment

--- myblog/settings.py
+++ myblog/settings.py
@@ -76,13 +76,11 @@ WSGI_APPLICATION = 'myblog.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f'sqlite:///{os.path.join(BASE_DIR, "db.sqlite3")}',
+        conn_max_age=600
+    )
 }
-
-DATABASES['default'] = dj_database_url.config(default='sqlite:///db.sqlite3')
 
 
 # Password validation
